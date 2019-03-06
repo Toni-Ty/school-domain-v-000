63e0fda3c17fda4b0cdf31f72d4b20974823028d
Toni-Ty <antonique2479@yahoo.com>
2019-03-06 06:24:58 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -7,13 +7,14 @@ class School
   end
 
   def add_student(student_name, grade)
-    roster[grade] || = []
+    roster[grade] ||= []
     roster[grade] << student_name
 
 
   end
 
-  def grade(grade)
+  def grade(student_grade)
+    roster[student_grade]
   end
 
 end
